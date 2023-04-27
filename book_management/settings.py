02b5bbfa3475430e2d6719d1de417176bc9dc947
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-27 18:52:49 +0900

    テンプレートをリファクタリング

--- book_management/settings.py
+++ book_management/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from typing import List
 
@@ -58,7 +59,9 @@ ROOT_URLCONF = "book_management.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- divisions/views.py
+++ divisions/views.py
@@ -10,13 +10,16 @@ def list(request: HttpRequest) -> HttpResponse:
     # すべての部署をデータベースから取得
     divisions = Division.objects.all()
     # コンテキストを作成
-    context = {"division_list": divisions}
+    context = {
+        "title": "部署一覧",
+        "division_list": divisions,
+    }
     # コンテキストでテンプレートをレンダリング
     return render(request, "divisions/division_list.html", context)
 
 
 def detail(request: HttpResponse, code: str) -> HttpResponse:
-    """部署詳細ビュー
+    """部署詳細関数ビュー
 
     Args:
         code: 部署コード
@@ -25,4 +28,8 @@ def detail(request: HttpResponse, code: str) -> HttpResponse:
     # 部署コードから部署モデルインスタンスを取得
     division = get_object_or_404(Division, pk=code)
     # コンテキストを渡してテンプレートをレンダリング
-    return render(request, "divisions/division_detail.html", {"division": division})
+    return render(
+        request,
+        "divisions/division_detail.html",
+        {"title": "部署詳細", "division": division},
+    )
