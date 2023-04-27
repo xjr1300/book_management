2278943d497ebbde5086a58c230f69addbb50370
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-27 16:57:44 +0900

    部署モデルを実装

--- divisions/models.py
+++ divisions/models.py
@@ -1,3 +1,42 @@
+# django.db.modelsモジュールをインポート
 from django.db import models
 
-# Create your models here.
+
+class Division(models.Model):
+    """部署モデル
+
+    django.db.models.Modelを継承したDivisionモデルを定義します。
+    """
+
+    # 部署コード
+    code = models.CharField(primary_key=True, max_length=2)
+    # 部署名
+    name = models.CharField(max_length=80)
+    # 作成日時
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 更新日時
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        """部署モデルインスタンス名を返却する。
+
+        Returns:
+            部署モデルインスタンス名。
+        """
+        return self.name
+
+    def full_name(self) -> str:
+        """部署モデルインスタンスのフルネームを返却する。
+
+        Returns:
+            部署モデルインスタンスのフルネーム。
+        """
+        return f"{self.code}: {self.name}"
+
+    def name_len(self) -> int:
+        """部署名の文字数を返却する。
+
+        Returns:
+            部署名の文字数。
+        """
+        return len(self.name)
--- divisions/views.py
+++ divisions/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpRequest, HttpResponse
 
 
 def index(request: HttpRequest) -> HttpResponse:
