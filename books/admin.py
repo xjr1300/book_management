f69a5dd8e505eb77c875782ae81ecb2796cafec8
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-28 10:16:36 +0900

    書籍アプリのモデルを管理サイトに登録

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, Classification, ClassificationDetail
+
+
+class ClassificationAdmin(admin.ModelAdmin):
+    """書籍分類モデルアドミン"""
+
+    list_display = (
+        "code",
+        "name",
+        "created_at",
+        "updated_at",
+    )
+
+
+class ClassificationDetailAdmin(admin.ModelAdmin):
+    """書籍分類詳細モデルアドミン"""
+
+    list_display = (
+        "code",
+        "classification",
+        "name",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("classification",)
+
+
+class BookAdmin(admin.ModelAdmin):
+    """書籍モデルアドミン"""
+
+    list_display = (
+        "title",
+        "classification_detail",
+        "authors",
+        "published_at",
+        "division",
+    )
+    list_filter = (
+        "classification_detail__classification",
+        "division",
+    )
+    search_fields = (
+        "title",
+        "classification_detail__classification__name",
+        "classification_detail__name",
+        "authors",
+        "division__name",
+    )
+
+
+admin.site.register(Classification, ClassificationAdmin)
+admin.site.register(ClassificationDetail, ClassificationDetailAdmin)
+admin.site.register(Book, BookAdmin)
