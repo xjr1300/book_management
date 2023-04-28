7c124ad2ee731bea1b1f377865735ffe2f2c1d33
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-28 09:37:06 +0900

    書籍分類詳細モデルを実装

--- books/models.py
+++ books/models.py
@@ -31,3 +31,41 @@ class Classification(TimestampModel):
             書籍分類モデルのフルネーム。
         """
         return f"{self.code}: {self.name}"
+
+
+class ClassificationDetail(TimestampModel):
+    """書籍分類詳細モデル"""
+
+    # 書籍分類詳細コード
+    code = models.CharField("書籍分類詳細コード", primary_key=True, max_length=3)
+    # 書籍分類
+    classification = models.ForeignKey(
+        Classification,
+        db_column="classification_code",
+        on_delete=models.PROTECT,
+        related_name="classification_details",
+        verbose_name="書籍分類詳細",
+    )
+    # 書籍分類詳細名
+    name = models.CharField("書籍分類詳細名", max_length=80)
+
+    class Meta:
+        db_table = "classification_details"
+        verbose_name = verbose_name_plural = "書籍分類詳細"
+        ordering = ("code",)
+
+    def __str__(self) -> str:
+        """書籍分類詳細モデルインスタンス名を返却する。
+
+        Returns:
+            書籍分類詳細モデルインスタンス名。
+        """
+        return self.name
+
+    def full_name(self) -> str:
+        """書籍分類詳細モデルインスタンスのフルネームを返却する。
+
+        Returns:
+            書籍分類詳細モデルのフルネーム。
+        """
+        return f"{self.code}: {self.name}"
