de3162ae9167640eb63c1a7168c87a8a8ae6b212
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-28 09:50:22 +0900

    ULIDFieldを実装

--- core/models.py
+++ core/models.py
@@ -11,3 +11,14 @@ class TimestampModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class ULIDField(models.CharField):
+    """ULIDモデルフィールド"""
+
+    def __init__(self, *args, **kwargs) -> None:
+        kwargs["max_length"] = 26
+        super().__init__(*args, **kwargs)
+
+    def db_type(self, connection) -> str:
+        return "char(26)"
