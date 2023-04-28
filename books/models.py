5f1d2411fef3234007cb3659fcbbaf8efa3f3b83
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-28 09:54:43 +0900

    書籍モデルを実装

--- books/models.py
+++ books/models.py
@@ -1,6 +1,8 @@
+import ulid
+
 from django.db import models
 
-from core.models import TimestampModel
+from core.models import TimestampModel, ULIDField
 
 
 class Classification(TimestampModel):
@@ -69,3 +71,53 @@ class ClassificationDetail(TimestampModel):
             書籍分類詳細モデルのフルネーム。
         """
         return f"{self.code}: {self.name}"
+
+
+class Book(TimestampModel):
+    """書籍モデル"""
+
+    # 書籍ID
+    id = ULIDField("書籍ID", primary_key=True, editable=False, default=ulid.ULID)
+    # タイトル
+    title = models.CharField("タイトル", max_length=120)
+    # 書籍分類詳細
+    classification_detail = models.ForeignKey(
+        ClassificationDetail,
+        db_column="classification_detail_code",
+        on_delete=models.PROTECT,
+        related_name="classification_detail_books",
+        verbose_name="書籍分類詳細",
+    )
+    # 著者または訳者
+    authors = models.TextField("著者または訳者", null=True, blank=True)
+    # ISBN
+    isbn = models.CharField("ISBN", max_length=40, null=True, blank=True)
+    # 出版社
+    publisher = models.CharField("出版社", max_length=80, null=True, blank=True)
+    # 発行日
+    published_at = models.DateField("発行日", null=True, blank=True)
+    # 管理部署コード
+    division = models.ForeignKey(
+        "divisions.Division",
+        db_column="division_code",
+        on_delete=models.PROTECT,
+        related_name="books",
+        verbose_name="管理部署",
+    )
+    # 廃棄済み
+    disposed = models.BooleanField("廃棄済み", default=False)
+    # 廃棄日
+    disposed_at = models.DateField("廃棄日", null=True, blank=True)
+
+    class Meta:
+        db_table = "books"
+        verbose_name = verbose_name_plural = "書籍"
+        ordering = ("id",)
+
+    def __str__(self) -> str:
+        """書籍のタイトルを返却する。
+
+        Returns:
+            書籍のタイトル。
+        """
+        return self.title
