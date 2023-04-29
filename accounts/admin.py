417fda80293513d132e8f3b8ab05fb5f5b7d8fdc
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-29 19:42:44 +0900

    ユーザーモデルを管理サイトに登録

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.forms import UserChangeForm as DjangoUserChangeForm
+from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class UserCreationForm(DjangoUserCreationForm):
+    """ユーザー作成フォーム"""
+
+
+class UserChangeForm(DjangoUserChangeForm):
+    """ユーザー作成フォーム"""
+
+
+class UserAdmin(DjangoUserAdmin):
+    """ユーザーアドミン"""
+
+    fieldsets = (
+        (None, {"fields": ("email", "password", "name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    add_form = UserCreationForm
+    form = UserChangeForm
+    list_display = (
+        "email",
+        "name",
+        "is_active",
+        "date_joined",
+    )
+    list_filter = (
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "groups",
+    )
+    search_fields = ("email", "name")
+    ordering = ("email",)
+
+
+admin.site.register(User, UserAdmin)
