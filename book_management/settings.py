37a5a10560e32d69ef0d47cb9f53c7c739273e92
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-29 19:19:28 +0900

    アカウントアプリを追加

--- book_management/settings.py
+++ book_management/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_bootstrap5",
     "debug_toolbar",
+    "accounts.apps.AccountsConfig",
     "divisions.apps.DivisionsConfig",
     "books.apps.BooksConfig",
 ]
