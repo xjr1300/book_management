48bef78bcd8604ce517290008e964c4f2feeb194
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-30 16:24:43 +0900

    Django REST Frameworkをインストール

--- book_management/settings.py
+++ book_management/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "django_bootstrap5",
     "debug_toolbar",
     "accounts.apps.AccountsConfig",
