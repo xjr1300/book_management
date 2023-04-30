2b539c7bdca0cd28b8f85fd6b96636f95df57186
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-04-30 14:18:57 +0900

    メッセージを提供する独自のLoginRequiredMixinを実装

--- core/mixins.py
+++ core/mixins.py
@@ -1,5 +1,8 @@
 from typing import Any, Dict
 
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin as DjangoLoginRequiredMixin
+from django.http import HttpRequest, HttpResponse
 from django.views import generic
 
 
@@ -23,3 +26,23 @@ class FormActionMixin(generic.base.ContextMixin):
         ctx = super().get_context_data(**kwargs)
         ctx["action"] = self.action
         return ctx
+
+
+class LoginRequiredMixin(DjangoLoginRequiredMixin):
+    """ログイン要求ミックスイン
+
+    Djangoが提供するLoginRequiredMixinを拡張して、ログインしていないユーザーがページにアクセスする
+    権限がないことを示すメッセージを、Djangoが適用するセッションフレームワークで通知できるようにします。
+    このミックスインを継承するビューは、permission_denied_messageをオーバーライドすることで、ビューに
+    あわせたメッセージを通知できます。
+    """
+
+    permission_denied_message = "ページにアクセスする権限がありません。"
+
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if not request.user.is_authenticated:
+            messages.add_message(
+                request, messages.WARNING, self.permission_denied_message
+            )
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
