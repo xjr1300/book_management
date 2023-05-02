09012e2b2589efe92eb0ac94ee21841490a149ee
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-05-02 19:25:59 +0900

    書籍分類詳細APIを実装

--- api1/urls.py
+++ api1/urls.py
@@ -6,4 +6,12 @@ from .books import views
 urlpatterns = [
     path("books/classifications/", views.classification_list),
     path("books/classifications/<str:code>/", views.classification_detail),
+    path(
+        "books/classification-details/",
+        views.ClassificationDetailListCreateView.as_view(),
+    ),
+    path(
+        "books/classification-details/<str:code>/",
+        views.ClassificationRetrieveUpdateDestroyView.as_view(),
+    ),
 ]
