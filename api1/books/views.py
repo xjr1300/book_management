fb7db0d05c98fb2078de2623111a82d365727eb8
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-05-03 13:33:15 +0900

    import文を修正

--- api1/books/views.py
+++ api1/books/views.py
@@ -5,13 +5,10 @@ from rest_framework.response import Response
 
 from books.models import Book, Classification, ClassificationDetail
 
-from .serializers import (
-    BookReadOnlySerializer,
-    BookWriteOnlySerializer,
-    ClassificationDetailSerializer,
-    ClassificationDetailUpdateSerializer,
-    ClassificationSerializer,
-)
+from .serializers import (BookReadOnlySerializer, BookWriteOnlySerializer,
+                          ClassificationDetailSerializer,
+                          ClassificationDetailUpdateSerializer,
+                          ClassificationSerializer)
 
 
 @api_view(["GET", "POST"])
