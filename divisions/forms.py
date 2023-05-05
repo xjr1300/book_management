c27fcc4c546f446d99f56b5b4d71a1577d264ece
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-05-05 20:09:47 +0900

    部署フォームの実装を削除

--- divisions/forms.py
+++ divisions/forms.py
@@ -1,11 +1,11 @@
-from django import forms
-
-from .models import Division
-
-
-class DivisionForm(forms.ModelForm):
-    """部署フォーム"""
-
-    class Meta:
-        model = Division
-        fields = ["code", "name"]
+# from django import forms
+#
+# from .models import Division
+#
+#
+# class DivisionForm(forms.ModelForm):
+#     """部署フォーム"""
+#
+#     class Meta:
+#         model = Division
+#         fields = ["code", "name"]
